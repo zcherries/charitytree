366780682af5a1852d25b099050fe659c7afceb4
Zach Herries <hotziggity@gmail.com>
2015-12-03 15:27:41 -0800

    feat/searchPage install webpack for react es6

--- client/main.js
+++ client/main.js
@@ -2,11 +2,23 @@
  * Created by Zach on 12/2/15.
  */
 "use strict";
-//const React = require('react');
-//const ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+    <p>
+    Hello, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()}
+    </p>
+    );
+  }
+});
+
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <HelloWorld date={new Date()} />,
   document.getElementById('container')
 );
 //const RaisedButton = require('material-ui/lib/raised-button');
---
+++ client/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: "./main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+    {test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel', query: {presets: ['react', 'es2015']}}
+    ]
+  }
+};
