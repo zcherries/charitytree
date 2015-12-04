4424fbfa882f69cf51d57da237a5462d840ea03e
git2go <git2go@users.noreply.github.com>
2015-12-03 19:27:32 -0800

    [feature] Implemented database schema

--- server/db/schemas/donor.js
+++ server/db/schemas/donor.js
@@ -1,13 +1,31 @@
 var mongoose = require('mongoose');
-var ObjectId = Schema.Types.ObjectId;
 
-var Donor = new mongoose.Schema({
-  name: { first: String, last: String },
-  email: String,
-  password: String,
+// var Schema = require('mongoose').Schema;
+// var model = require('mongoose').model;
+
+var Schema = mongoose.Schema, ObjectId = Schema.Types.ObjectId;
+
+var endorsementSchema = new Schema({
+  review: { type: String, trim: true },
+  rating: Number,
+  org: { type: ObjectId, ref: 'Organization' }
+});
+
+var DonorSchema = new Schema({
+  name: {
+    first: { type: String, trim: true },
+    last: { type: String, trim: true }
+  },
+  email: { type: String, required: true },
+  password: {type: String, required: true },
   signup_date: Date,
   profile_img: { data: Buffer, contentType: String },
   areas_of_focus: { type: [String], required: true },
-  projects: [ProjectSchema],
-  endorsements: [{ type: ObjectId, ref: 'Organization' }]
+  sponsored_projects: [{ type: ObjectId, ref: 'Project' }],
+  orgs_followed: [{ type: ObjectId, ref: 'Organization' }],
+  endorsements: [endorsementSchema]
 });
+
+var Donor = mongoose.model('Donor', DonorSchema);
+
+module.exports = Donor;
--- server/db/schemas/project.js
+++ server/db/schemas/project.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose');
+
+// var Schema = require('mongoose').Schema;
+// var model = require('mongoose').model;
+
 var Schema = mongoose.Schema, ObjectId = Schema.Types.ObjectId;
 
-var Project = new mongoose.Schema({
-  
-})
+var ProjectSchema = new Schema({
+  org: { type: ObjectId, ref: 'Organization' },
+  start_date: Date,
+  end_date: Date,
+  status: String,
+  info: { type: String, trim: true, required: true },
+  areas_of_focus: { type: [String], required: true },
+  amount: {
+    goal: Number,
+    current: Number
+  },
+  sponsors: [{ type: ObjectId, ref: 'Donor' }],
+  media: [ObjectId]
+});
+
+var Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = Project;
