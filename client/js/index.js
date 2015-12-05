0136be7023589594d4390748664fffdc372cc692
Zach Herries <hotziggity@gmail.com>
2015-12-04 21:38:24 -0800

    feat/search.js complete navbar and search bar
    also reorganized files

--- client/js/index.js
+++ client/js/index.js
@@ -1,15 +1,16 @@
 "use strict";
 import React from 'react';
 import { render } from 'react-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import StaticContainer from 'react-static-container';
 import { Router, Route, Link, IndexRoute } from 'react-router';
 import { createHistory, useBasename } from 'history';
 
 /*local imports*/
-import {Browse} from './js/browsePage.js';
-import {Options} from './js/browsePage.js';
-import {Search} from './js/search.js';
+import {Browse} from './browsePage.js';
+import {Options} from './browsePage.js';
+import {Footer} from './footer.js';
+import {Navbar} from './navbar.js';
+import {Search} from './search.js';
+
 
 const history = useBasename(createHistory)({
   basename: '/'
@@ -29,6 +30,10 @@ var Index = React.createClass({
   render: function() {
     return(
       <div>
+        {/*Navbar*/}
+        <Navbar />
+
+        {/*Parallax*/}
         <div className="parallax-container">
           <div className="parallax"><img src="http://www.muslimpress.com/wp-content/uploads/2015/09/45.jpg" /></div>
         </div>
@@ -41,46 +46,14 @@ var Index = React.createClass({
         <div className="parallax-container">
           <div className="parallax"><img src="http://117.240.88.108/rockys/wp-content/uploads/2012/05/charity-day.jpg" /></div>
         </div>
-
+        {/*Footer*/}
         <Footer />
       </div>
     );
   }
 });
 
-var Footer = React.createClass({
-  render: function () {
-    return (
-      <footer className="page-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col l6 s12">
-              <h5 className="white-text">Footer Content</h5>
-              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-            </div>
-            <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Links</h5>
-              <ul>
-                <li><Link to="search" className="grey-text text-lighten-3">Search</Link></li>
-                <li><Link to="browse" className="grey-text text-lighten-3">Browse</Link></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="footer-copyright">
-          <div className="container">
-            © 2014 Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-          </div>
-        </div>
-      </footer>
-    );
-  }
-})
-
-ReactDOM.render((
+render((
   <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Index} />
