4719b11e033a2618e85f3bf0b70188e158bb0441
git2go <git2go@users.noreply.github.com>
2015-12-04 18:11:00 -0800

    [update] Added controllers

---
+++ server/db/controllers/organization.js
@@ -0,0 +1,76 @@
+var Org = require('../models/organization.js');
+
+module.exports = {
+  retrieve: function(req, res, next, criteria, options, method) {
+    var criteria = criteria || {}, method = method || '';
+    var query = (Org[method]) ? Org[method](criteria) : Org.find(criteria);
+
+    if (options) {
+      for (var key in options) {
+        if (query[key]) {
+          query[key](options[key]);
+        }
+      }
+    }
+    query.exec(function(err, orgs) {
+      if (err) handleError(req, res, "Controller: Organization, Method: Retrieve", err);
+      res.send({ status: 200, results: orgs });
+    });
+  },
+
+  create: function(req, res, next, orgData) {
+    Org.findOne({name: orgData.name}, function(err, found) {
+      if (err) handleError(req, res, "Controller create", err);
+      if (!found) {
+        Org.create(orgData, function(err, org) {
+          if (err) handleError(req, res, "Controller create", err);
+          res.send({ status: 201, results: org });
+        });
+      } else {
+          res.status(400).send({ status: 400, message: "We found an organization with this name." });
+      }
+    });
+  },
+
+  update: function(req, res, next, criteria, changes, method, options) {
+    Org.findOne(criteria, function(err, doc) {
+      if (err) handleError(req, res, "Controller update", err);
+      if (doc) {
+        for (var key in changes) {
+          if (doc[key]) {
+            doc[key] = changes[key];
+          }
+        }
+        doc.save(function(err, org) {
+          res.send({ status: 201, results: org });
+        });
+      } else {
+        res.status(400).send({ status: 400, message: "Organization not found." });
+      }
+    });
+  },
+
+  delete: function(req, res, next, criteria, options, method) {
+    var criteria = criteria || {}, method = method || '';
+    var query = (Org[method]) ? Org[method](criteria) : Org.findOne(criteria);
+    if (options) {
+      for (var key in options) {
+        if (query[key])
+          query[key](options[key]);
+      }
+    }
+    query.remove().exec(function(err, org) {
+      if (err)
+        handleError(req, res, "Controller: Organization, Method: Delete", err);
+      else
+        res.send({ status: 201, message: "Delete was successful" });
+    });
+  }
+};
+
+var handleError = function(req, res, source, err, message) {
+  if (req.method === 'GET')
+    res.status(400).send({ status: 400, message: message });
+  if (req.method === 'POST')
+    res.status(400).send({ status: 400, results: null, message: message });
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var mongoose_models = require('./db/schemas');
-var db = require('./db/connection.js');
+var org = require('./db/controllers');
+var connection = require('./db/connection.js');
 
 var IP = '127.0.0.1', PORT = 8000;
 
@@ -8,12 +8,12 @@ var app = express();
 
 app.use(express.static(__dirname + '/../' + 'client'));
 
-app.get('/', function(req, res, next) {
-  mongoose_models.donor.find({}, function(err, donors) {
-    if (err) { throw err; }
-    console.log("Donors: ", donors);
-    res.send('index.html');
-  });
+app.get('/organizations', function(req, res, next) {
+  // var orgData = { name: 'Red Cross', about: 'We are Red Cross', signup_date: '12/4/2015' }
+  // org.create(req, res, next, orgData);
+  // org.update(req, res, next, {name: 'Red Cross'}, {name: 'The Red Cross'});
+  // org.retrieve(req, res, next, {}, { select: 'about name', sort: {signup_date: -1}});
+  // org.delete(req, res, next, {name: "The Red Cross"}, {}, 'find');
 });
 
 app.listen(PORT, IP);
