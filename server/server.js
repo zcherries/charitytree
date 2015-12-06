b86abc3918388477d59059016fce13e821cd1006
git2go <git2go@users.noreply.github.com>
2015-12-05 18:10:51 -0800

    [test] Added more test data

--- server/server.js
+++ server/server.js
@@ -3,28 +3,43 @@ var path = require('path');
 var Controller = require('./db/controllers');
 var Model = require('./db/models');
 var connection = require('./db/connection.js');
+var xray = require('x-ray');
+var x = new xray();
+// var promisify = require("promisify-node");
+var fs = require('fs');
+var IP = '127.0.0.1', PORT = 8000;
 
-// var fs = require('fs');
 // var rl = require('readline').createInterface({
-//   input: require('fs').createReadStream('./countriesoftheworld.txt')
+//  input: require('fs').createReadStream('./areas_of_focus.txt')
 // });
 //
-// rl.on('line', function(line) {
-//   Model.AoF.create({name: line}, function(err, country) {
-//     // console.log('Line from file:', line)
+// rl.on('line', function(aof) {
+//   Model.AoF.findOne({name: aof}, function(err, match) {
+//     if (!match) {
+//       console.log(aof)
+//       Model.AoF.create({name: aof}, function(err, line) {
+//         console.log('Line from file:', line);
+//       });
+//     }
 //   });
 // });
 
-var IP = '127.0.0.1', PORT = 8000;
-
 var app = express();
-
 app.use(express.static(__dirname + '/../client'));
 
-app.get('/organizations', function(req, res, next) {
-
-});
-
+// var arrLinks = [];
+// app.get('/organizations', function(req, res, next) {
+//   x('http://www.unodc.org/ngo/list.jsp', 'tr td p', ['a@href'], [{
+//     href: '@href'
+//   }])(function(err, links) {
+//     arrLinks = links.map(function(link) {
+//       if (link != null)
+//         return link;
+//     });
+//     res.send(arrLinks);
+//   })
+//   // res.send('index.html')
+// });
 app.get('/aofs', function(req, res, next) {
   Controller.AoF.retrieve(req, res, next);
 });
