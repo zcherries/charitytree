d0f956eef26b330a3f5c580b2053f79a64ce1af0
git2go <git2go@users.noreply.github.com>
2015-12-07 17:15:48 -0800

    [refactor] Server folder cleanup

---
+++ server/resources/projects.js
@@ -0,0 +1,14 @@
+var projects = module.exports = [
+  {
+    org: "",
+    info: "",
+    start_date: "",
+    end_date: "",
+    status: "Pending",
+    areas_of_focus: [],
+    amount: {
+      goal: ,
+      current:
+    }
+  }
+]
--- server/server.js
+++ server/server.js
@@ -3,30 +3,12 @@ var path = require('path');
 var Controller = require('./db/controllers');
 var Model = require('./db/models');
 var connection = require('./db/connection.js');
-//var xray = require('x-ray');
-//var x = new xray();
-// var promisify = require("promisify-node");
-var fs = require('fs');
-var IP = '127.0.0.1', PORT = 4000;
-
-// var rl = require('readline').createInterface({
-//  input: require('fs').createReadStream('../areas_of_focus.txt')
-// });
-
-// rl.on('line', function(aof) {
-//   Model.AoF.findOne({name: aof}, function(err, match) {
-//     if (!match) {
-//       console.log(aof)
-//       Model.AoF.create({name: aof}, function(err, line) {
-//         console.log('Line from file:', line);
-//       });
-//     }
-//   });
-// });
-
 var organizations = require('./resources/organizations.js')
 
 var app = express();
+
+var IP = '127.0.0.1', PORT = 4000;
+
 app.use(express.static(__dirname + '/../client'));
 
 app.get('/organizations', function(req, res, next) {
