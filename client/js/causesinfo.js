0c0006ebf8d025525a136f58fd00d28a97af50e7
Zach Herries <hotziggity@gmail.com>
2015-12-11 12:16:31 -0800

    feat/projectCreate_startedBuildingClasses

--- client/js/causesinfo.js
+++ client/js/causesinfo.js
@@ -4,116 +4,116 @@ exports.CausesInfo = [
     title: "Animals",
     img: "https://c1.staticflickr.com/5/4119/4931607222_92bf5fea5f_b.jpg",
     subcauses: [
-      {title: "Right", tags: "Animal Rights"},
-      {title: "Services", tags: "Animal Services"},
-      {title: "Welfare", tags: "Animal Welfare"},
-      {title: "Wildlife Conservation", tags: "Wildlife Conservation"},
-      {title: "Zoos & Aquariums", tags: "Zoos Aquariums"} ]
+      {id: "animal_rights", title: "Right", tags: "Animal Rights"},
+      {id: "animal_services", title: "Services", tags: "Animal Services"},
+      {id: "animal_welfare", title: "Welfare", tags: "Animal Welfare"},
+      {id: "wildlife", title: "Wildlife Conservation", tags: "Wildlife Conservation"},
+      {id: "zoos_aquariums", title: "Zoos & Aquariums", tags: "Zoos Aquariums"} ]
   },
   {
     id: "arts",
     title: "Arts, Culture, Humanitites",
     img: "https://c1.staticflickr.com/5/4077/4931032081_9d5ca8cfff_b.jpg",
     subcauses: [
-      {title: "Libraries", tags: "Libraries"},
-      {title: "Historical Societies", tags: "Historical Societies"},
-      {title: "Landmark Preservation", tags: "Landmark Preservation"},
-      {title: "Museums", tags: "Museums"},
-      {title: "Performing Arts", tags: "Performing Arts"},
-      {title: "Public Broadcasting and Media", tags: "Public Broadcasting Media"} ]
+      {id: "libraries", title: "Libraries", tags: "Libraries"},
+      {id: "historical_societies", title: "Historical Societies", tags: "Historical Societies"},
+      {id: "landmark_preservation", title: "Landmark Preservation", tags: "Landmark Preservation"},
+      {id: "museums", title: "Museums", tags: "Museums"},
+      {id: "performing_arts", title: "Performing Arts", tags: "Performing Arts"},
+      {id: "public_broadcasting", title: "Public Broadcasting and Media", tags: "Public Broadcasting Media"} ]
   },
   {
     id: "community",
     title: "Community Development",
     img: "https://c1.staticflickr.com/9/8082/8380083742_098a5616b9_b.jpg",
     subcauses: [
-      {title: "Fundraising", tags: "Fundraising"},
-      {title: "Community Foundations", tags: "Community Foundations"},
-      {title: "Housing and Neighborhood Development", tags: "Housing Development"} ]
+      {id: "fundraising", title: "Fundraising", tags: "Fundraising"},
+      {id: "community_foundations", title: "Community Foundations", tags: "Community Foundations"},
+      {id: "housing", title: "Housing and Neighborhood Development", tags: "Housing Development"} ]
   },
   {
     id: "education",
     title: "Education",
     img: "https://c1.staticflickr.com/5/4123/4931585570_d75507cee3_b.jpg",
     subcauses: [
-      {title: "Universities, Graduate Schools, and Technological Institutes", tags: "Universities Graduate Technological"},
-      {title: "Private Elementary and Secondary Schools", tags: "Private Elementary Secondary Schools"},
-      {title: "Private Liberal Arts Colleges", tags: "Private Liberal Arts Colleges"},
-      {title: "Other Education Programs and Services", tags: "Other Education"} ]
+      {id: "higher_education", title: "Universities, Graduate Schools, and Technological Institutes", tags: "Universities Graduate Technological"},
+      {id: "child_education", title: "Private Elementary and Secondary Schools", tags: "Private Elementary Secondary Schools"},
+      {id: "private_education", title: "Private Liberal Arts Colleges", tags: "Private Liberal Arts Colleges"},
+      {id: "other_education", title: "Other Education Programs and Services", tags: "Other Education"} ]
   },
   {
     id: "environment",
     title: "Environment",
     img: "https://c1.staticflickr.com/5/4099/4931030575_033fca9b28_b.jpg",
     subcauses: [
-      {title: "Environmental Protection and Conservation", tags: "Environment Protection Conservation"},
-      {title: "Botanical Gardens, Parks, and Nature Centers", tags: "Parks Nature Botanical Gardens"} ]
+      {id: "environment_protection_conservation", title: "Environmental Protection and Conservation", tags: "Environment Protection Conservation"},
+      {id: "parks", title: "Botanical Gardens, Parks, and Nature Centers", tags: "Parks Nature Botanical Gardens"} ]
   },
   {
     id: "health",
     title: "Health",
     img: "https://c1.staticflickr.com/5/4119/4931607222_92bf5fea5f_b.jpg",
     subcauses: [
-      {title: "Diseases, Disorders, and Disciplines", tags: "Diseases Disorders Disciplines"},
-      {title: "Patient and Family Support", tags: "Patient Family Support"},
-      {title: "Health Care Facilities and Programs", tags: "Health Care"},
-      {title: "Mental Health and Crisis Services", tags: "Mental Health Crisis"},
-      {title: "Treatment Services", tags: "Health Treatment"},
-      {title: "Prevention Services", tags: "Health Prevention"},
-      {title: "Medical Research", tags: "Medical Research"} ]
+      {id: "diseases", title: "Diseases, Disorders, and Disciplines", tags: "Diseases Disorders Disciplines"},
+      {id: "patient_family_support", title: "Patient and Family Support", tags: "Patient Family Support"},
+      {id: "health_care", title: "Health Care Facilities and Programs", tags: "Health Care"},
+      {id: "mental_health_crisis", title: "Mental Health and Crisis Services", tags: "Mental Health Crisis"},
+      {id: "health_treatment", title: "Treatment Services", tags: "Health Treatment"},
+      {id: "health_prevention", title: "Prevention Services", tags: "Health Prevention"},
+      {id: "medical_research", title: "Medical Research", tags: "Medical Research"} ]
   },
   {
     id: "services",
     title: "Human Services",
     img: "https://c1.staticflickr.com/5/4115/4931010827_98907c06c2_b.jpg",
     subcauses: [
-      {title: "Children's and Family Services", tags: "Children Family"},
-      {title: "Crime and Legal Related", tags: "Crime Legal"},
-      {title: "Youth Development", tags: "Youth Development"},
-      {title: "Shelter", tags: "Shelter"},
-      {title: "Crisis Services", tags: "Crisis"},
-      {title: "Food Distribution", tags: "Food Distribution"},
-      {title: "Food Banks, and Food Pantries", tags: "Food Banks Pantries"},
-      {title: "Multipurpose Human Service Organizations", tags: "Multipurpose"},
-      {title: "Public Safety, Disaster Preparedness, and Relief", tags: "Disaster Preparedness Safety Relief"},
-      {title: "Homeless Services", tags: "Homeless"},
-      {title: "Social Services", tags: "Social"} ]
+      {id: "children_family", title: "Children's and Family Services", tags: "Children Family"},
+      {id: "crime_legal", title: "Crime and Legal Related", tags: "Crime Legal"},
+      {id: "youth_development", title: "Youth Development", tags: "Youth Development"},
+      {id: "shelter", title: "Shelter", tags: "Shelter"},
+      {id: "crisis", title: "Crisis Services", tags: "Crisis"},
+      {id: "food_distribution", title: "Food Distribution", tags: "Food Distribution"},
+      {id: "food_banks_pantries", title: "Food Banks, and Food Pantries", tags: "Food Banks Pantries"},
+      {id: "multipurpose", title: "Multipurpose Human Service Organizations", tags: "Multipurpose"},
+      {id: "disaster_prep_safety", title: "Public Safety, Disaster Preparedness, and Relief", tags: "Disaster Preparedness Safety Relief"},
+      {id: "homeless", title: "Homeless Services", tags: "Homeless"},
+      {id: "social", title: "Social Services", tags: "Social"} ]
   },
   {
     id: "rights",
     title: "Human and Civil Rights",
     img: "https://c1.staticflickr.com/5/4099/4931620392_e3654db7dd_b.jpg",
     subcauses: [
-      {title: "Advocacy and Education", tags: "Rights Advocacy"},
-      {title: "Social Services", tags: "Social Services"},
-      {title: "Human Trafficking", tags: "Human Trafficking"} ]
+      {id: "rights_advocacy", title: "Advocacy and Education", tags: "Rights Advocacy"},
+      {id: "social_services", title: "Social Services", tags: "Social Services"},
+      {id: "human_trafficking", title: "Human Trafficking", tags: "Human Trafficking"} ]
   },
   {
     id: "international",
     title: "International",
     img: "https://c2.staticflickr.com/4/3793/10569512605_3815d5c4f2_b.jpg",
     subcauses: [
-      {title: "Development and Relief Services", tags: "Development Relief"},
-      {title: "International Peace", tags: "International Peace"},
-      {title: "Security, and Affairs", tags: "Security Affairs"},
-      {title: "Humanitarian Relief Supplies", tags: "Humanitarian Relief Supplies"},
-      {title: "Foreign Charity Support Organizations", tags: "Foreign"} ]
+      {id: "development_relief", title: "Development and Relief Services", tags: "Development Relief"},
+      {id: "international_peace", title: "International Peace", tags: "International Peace"},
+      {id: "security_affairs", title: "Security, and Affairs", tags: "Security Affairs"},
+      {id: "humanitarian_relief_supplies", title: "Humanitarian Relief Supplies", tags: "Humanitarian Relief Supplies"},
+      {id: "foreign", title: "Foreign Charity Support Organizations", tags: "Foreign"} ]
   },
   {
     id: "religion",
     title: "Religion",
     img: "https://c1.staticflickr.com/5/4073/4931004261_af5c021afb_b.jpg",
     subcauses: [
-      {title: "Religious Activities", tags: "Religious Activities"},
-      {title: "Religious Media & Broadcasting", tags: "Religious Media Broadcasting"} ]
+      {id: "religious_activities", title: "Religious Activities", tags: "Religious Activities"},
+      {id: "religious_media", title: "Religious Media & Broadcasting", tags: "Religious Media Broadcasting"} ]
   },
   {
     id: "research",
     title: "Research and Public Policy",
     img: "https://c1.staticflickr.com/5/4073/4931004261_af5c021afb_b.jpg",
     subcauses: [
-      {title: "Non-Medical Science & Technology Research", tags: "Science Technology Research"},
-      {title: "Medical Research", tags: "Medical Research"},
-      {title: "Social and Public Policy Research", tags: "Social Public Policy Research"} ]
+      {id: "science_technology_research", title: "Non-Medical Science & Technology Research", tags: "Science Technology Research"},
+      {id: "medical_research", title: "Medical Research", tags: "Medical Research"},
+      {id: "social_public_policy", title: "Social and Public Policy Research", tags: "Social Public Policy Research"} ]
   }
 ];
