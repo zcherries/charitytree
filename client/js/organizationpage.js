afc93f028caf0356b38a09bfc59ff3aa4c7f088b
Zach Herries <hotziggity@gmail.com>
2015-12-19 15:36:55 -0800

    feat/orgPageDesign

--- client/js/organizationpage.js
+++ client/js/organizationpage.js
@@ -118,60 +118,68 @@ var Organization = exports.Organization = React.createClass({
 
     return (
       <div className="container">
-        <h1 className="center-align">
-          {this.props.currentOrganization.name}
-        </h1>
+        {/*Header*/}
+        <div id="location" className="center-align section scrollspy">
+          <h1>
+            {this.props.currentOrganization.name}
+          </h1>
+          <i className="medium material-icons">room</i>
+          <h5> {this.props.currentOrganization.address}</h5>
+        </div>
         <div className="row">
           <div className="col s12 m10 l10">
             <div className="row">
-              <div className="col s12 m7">
-                <img className="responsive-img materialboxed" src="https://c1.staticflickr.com/5/4140/4930996357_8c6f018343_z.jpg"/>
+
+              {/*Image 1*/}
+              <div className="col s12">
+                <img className="center-image responsive-img materialboxed" src="https://c1.staticflickr.com/5/4140/4930996357_8c6f018343_z.jpg"/>
               </div>
 
               {/*Org Description*/}
-              <div id="description" className="col s12 m5 center-align section scrollspy">
-                <h6><i className="material-icons">location_on</i><pre>{this.props.currentOrganization.address}</pre></h6>
-                <i className="material-icons">description</i>
-                <h5> Description: {this.props.currentOrganization.about}</h5>
+              <div id="description" className="col s12 m10 push-m1 center-align section scrollspy flow-text">
+                <i className="medium material-icons space-above">description</i>
+                <h5> Description</h5>
+                <p>{this.props.currentOrganization.about}</p>
               </div>
-            </div>
 
             {/*Additional Info*/}
-            <div className="row">
-              <div className="col s6 valign-wrapper">
-                <h3 className="valign">
-                  Additional info here about what the Org Cares about
-                  Boom and there goes the dynamite
-                </h3>
+              <div className="col s12 m5 l6">
+                <blockquote>Additional info here about what the Org Cares about. Boom and there goes the dynamite</blockquote>
               </div>
 
               {/*Image 2*/}
-              <div className="col s6">
-                <img className="responsive-img materialboxed" src="https://c1.staticflickr.com/5/4116/4931019303_2f386bffb7_z.jpg"/>
+              <div className="col s12 m7 l6">
+                <img className="responsive-img materialboxed circle" src="https://c1.staticflickr.com/5/4116/4931019303_2f386bffb7_z.jpg"/>
               </div>
+            </div>
 
+            <div className="row">
               {/*Areas of Focus*/}
-              <div id="aofs" className="section scrollspy">
-                <h4>Areas of focus:</h4>
+              <div id="aofs" className="col s12 m10 push-m1 pull-m1 center-align section scrollspy">
+                <i className="medium material-icons space-above">location_searching</i>
+                <h4> Areas of focus:</h4>
                 {aofs}
               </div>
 
               {/*Current Project*/}
-              <div id="current-projects" className="section scrollspy">
-                <h2>Our Current Projects:</h2>
-                {currentProjects}
+              <div id="current-projects" className="col s12 center-align section scrollspy">
+                <i className="medium material-icons space-above">assignment_late</i>
+                <h2> Our Current Projects:</h2>
+                <div className="left-align">{currentProjects}</div>
               </div>
 
               {/*Past Projects*/}
-              <div id="past-projects" className="section scrollspy">
-                <h2>Our Past Projects:</h2>
-                <div>{pastProject}</div>
+              <div id="past-projects" className="col s12 center-align section scrollspy">
+                <i className="medium material-icons space-above">assignment_turned_in</i>
+                <h2> Our Past Projects:</h2>
+                <div className="left-align">{pastProject}</div>
               </div>
 
               {/*Endorsements*/}
-              <div id="endorsements" className="section scrollspy">
-                <h2>Endorsements:</h2>
-                <div>Various Endorsements</div>
+              <div id="endorsements" className="col s12 center-align section scrollspy">
+                <i className="medium material-icons space-above">verified_user</i>
+                <h2> Endorsements:</h2>
+                <div className="left-align">Various Endorsements</div>
               </div>
 
             </div>
@@ -192,6 +200,11 @@ var ScrollSpyListItems = React.createClass({
       <div className="toc-wrapper pinned" >
         <div>
           <ul className="section table-of-contents">
+            <li>
+              <a href="#location">
+                Location
+              </a>
+            </li>
             <li>
               <a href="#description">
                 Description
