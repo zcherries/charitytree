ad6d3a46f6e2ccd47a31fa48a0e3d11d9b1413a6
Zach Herries <hotziggity@gmail.com>
2015-12-28 20:01:12 -0800

    bugFix/fixOrgConnections

--- client/js/project.js
+++ client/js/project.js
@@ -6,18 +6,12 @@ import { CausesInfo } from './causesinfo.js';
 var LocalStorageMixin = require('react-localstorage');
 
 //var ReactIntl = require('react-intl');
-//
 //var IntlMixin       = ReactIntl.IntlMixin;
 //var FormattedNumber = ReactIntl.FormattedNumber;
 
 var Project = exports.Project = React.createClass ({
   displayName: 'Project',
   mixins: [ History,LocalStorageMixin ],
-  componentWillMount: function(){
-    // console.log('on project page and props.project is ', this.props.currentProject);
-    // console.log('on proj page and locStocurProjObj is ', JSON.parse(localStorage.getItem('currProjObj'));
-    // this.setCurrentOrg();
-  },
 
   getInitialState: function(){
     return {
@@ -26,20 +20,14 @@ var Project = exports.Project = React.createClass ({
   },
 
   componentDidMount: function () {
-    $('.materialboxed').materialbox();
-    $('ul.tabs').tabs();
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-
     console.log('inside of project at beginning of componentDidMount');
 
     $.ajax({
-        url:'/project_get/'+localStorage.currentProjID,
+        url:'/project_get/'+localStorage.projectId,
         // dataType: 'json',
         method: "GET",
         success: function (data) {
           console.log("on success in projdid with params.id and res.data is ", data.results);
-
-          // localStorage.setItem('currOrgObj', JSON.stringify(data.results));
 
           this.setState({
             project: data.results
@@ -51,8 +39,10 @@ var Project = exports.Project = React.createClass ({
           console.error(xhr, status, err.toString());
         }.bind(this)
       });
+    $('.materialboxed').materialbox();
+    $('ul.tabs').tabs('select_tab', 'tab_id');
 
-    ('inside of componentDidMount and state.project is ', this.state.project);
+    console.log('inside of componentDidMount and state.project is ', this.state.project);
   },
 
   setCurrentOrg: function(){
@@ -68,7 +58,8 @@ var Project = exports.Project = React.createClass ({
   },
 
   navToOrg: function(){
-    localStorage.setItem('currOrgObj', this.state.project._org._id);
+    console.log("Project/nTO/this.state.project._org.name:",this.state.project._org.name);
+    localStorage.setItem('currentOrganization', this.state.project._org._id);
     this.props.navigateToOrganizationPage();
   },
 
@@ -81,91 +72,91 @@ var Project = exports.Project = React.createClass ({
     //   }
     // }.bind(this));
     // project = project[0];
-    if(this.state.project){
+    if(this.state.project) {
 
-    var project = this.state.project;
-    // var org = JSON.parse(localStorage.getItem('currOrgObj'));
+      var project = this.state.project;
+      // var org = JSON.parse(localStorage.getItem('currOrgObj'));
 
-    var needs;
+      var needs;
 
-    console.log("Project/render/project.needs_list:",project.needs_list);
-    if (project.needs_list.length > 0) {
-      needs = project.needs_list.map(function (need, index) {
-        console.log("Project/render/needs/need:", need);
-        return (
-          <Needs
-            key={index}
-            title={need.title}
-            description={need.description}
-            cost={need.cost}
-            quantity_needed={need.quantity_needed}
-            number_purchased={need.number_purchased}/>
-        );
-      }.bind(this));
-    }
+      console.log("Project/render/project.needs_list:",project.needs_list);
+      if (project.needs_list.length > 0) {
+        needs = project.needs_list.map(function (need, index) {
+          console.log("Project/render/needs/need:", need);
+          return (
+            <Needs
+              key={index}
+              title={need.title}
+              description={need.description}
+              cost={need.cost}
+              quantity_needed={need.quantity_needed}
+              number_purchased={need.number_purchased}/>
+          );
+        }.bind(this));
+      }
 
-    var percentRaised = {width: (project.amount.current / project.amount.goal * 100) + "%"};
+      var percentRaised = {width: (project.amount.current / project.amount.goal * 100) + "%"};
 
-    return (
-      <div>
-        <div className="center-align">
-          <h3>{project.title}</h3>
-          <div onClick={this.navToOrg}>
-          <h4>the project.org is {this.state.project._org.name}</h4>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col s12 m8">
-            <img className="responsive-img materialboxed" src="http://worldofgoodethiopia.org/yahoo_site_admin/assets/images/30050052.182123348_std.jpg" />
-          </div>
-          <div className="col s12 m4">
-            <h3>Goal: ${project.amount.goal}</h3>
-            <h4>Progress: ${project.amount.current}</h4>
-            <div className="progress">
-              <div className="determinate" style={percentRaised}>
-              </div>
-            </div>
-            <h5>Number of donors: {project.total_donors_participating}</h5>
-            <h6>Status: {project.status}</h6>
-          </div>
-        </div>
-
-
-        <div className="row">
-          <div className="col s12 m4 push-m8">
-            {needs ? needs : "No needs to display"}
-          </div>
-          <div className="col s12 m8 pull-m4">
-
-            <div className="row">
-              <div className="col s12">
-                <ul className="tabs">
-                  <li className="tab col s3"><a href="#description">Description</a></li>
-                  <li className="tab col s3"><a href="#updates">Updates</a></li>
-                  <li className="tab col s3"><a href="#comments">Comments</a></li>
-                </ul>
-              </div>
-              <div id="description" className="col s12">{project.info}</div>
-              <div id="updates" className="col s12">
-                <h5>Updates</h5>
-                <p>Knausgaard PBR&B organic, pickled skateboard etsy freegan vice green juice tacos. Small batch YOLO gluten-free humblebrag etsy skateboard. Freegan normcore selvage stumptown williamsburg pinterest marfa. 90s ramps aesthetic, cliche farm-to-table kickstarter narwhal YOLO whatever small batch mustache. Schlitz mlkshk yr, etsy craft beer keffiyeh single-origin coffee. XOXO kickstarter flannel, fingerstache PBR&B tousled wayfarers kale chips ramps kitsch craft beer. Blue bottle put a bird on it deep v DIY, four loko retro distillery.
-                </p>
-              </div>
-
-              <div id="comments" className="col s12">
-                <h5>Comments</h5>
-                <p>Pabst kogi cardigan echo park raw denim helvetica. Mlkshk echo park PBR&B, dreamcatcher franzen forage iPhone blog pop-up four dollar toast fixie chartreuse VHS brunch leggings. Chillwave pitchfork forage venmo, pork belly irony authentic ugh. Literally gentrify banh mi pinterest hoodie. Typewriter migas mlkshk hoodie letterpress biodiesel post-ironic. Church-key pitchfork mlkshk, cardigan everyday carry fashion axe tofu. Intelligentsia DIY shabby chic, chartreuse cardigan occupy distillery tilde artisan kogi flexitarian readymade.
-                </p></div>
+      return (
+        <div>
+          <div className="center-align">
+            <h3>{project.title}</h3>
+            <div onClick={this.navToOrg}>
+              <h4>the project.org is {this.state.project._org.name}</h4>
             </div>
           </div>
-        </div>
-     </div>
-    );
-    }else{
+
+          <div className="row">
+            <div className="col s12 m8">
+              <img className="responsive-img materialboxed" src="http://worldofgoodethiopia.org/yahoo_site_admin/assets/images/30050052.182123348_std.jpg" />
+            </div>
+            <div className="col s12 m4">
+              <h3>Goal: ${project.amount.goal}</h3>
+              <h4>Progress: ${project.amount.current}</h4>
+              <div className="progress">
+                <div className="determinate" style={percentRaised}>
+                </div>
+              </div>
+              <h5>Number of donors: {project.total_donors_participating}</h5>
+              <h6>Status: {project.status}</h6>
+            </div>
+          </div>
+
+
+          <div className="row">
+            <div className="col s12 m4 push-m8">
+              {needs ? needs : "No needs to display"}
+            </div>
+            <div className="col s12 m8 pull-m4">
+
+              <div className="row">
+                <div className="col s12">
+                  <ul className="tabs">
+                    <li className="tab col s3"><a href="#description">Description</a></li>
+                    <li className="tab col s3"><a href="#updates">Updates</a></li>
+                    <li className="tab col s3"><a href="#comments">Comments</a></li>
+                  </ul>
+                </div>
+                <div id="description" className="col s12">{project.info}</div>
+                <div id="updates" className="col s12">
+                  <h5>Updates</h5>
+                  <p>Knausgaard PBR&B organic, pickled skateboard etsy freegan vice green juice tacos. Small batch YOLO gluten-free humblebrag etsy skateboard. Freegan normcore selvage stumptown williamsburg pinterest marfa. 90s ramps aesthetic, cliche farm-to-table kickstarter narwhal YOLO whatever small batch mustache. Schlitz mlkshk yr, etsy craft beer keffiyeh single-origin coffee. XOXO kickstarter flannel, fingerstache PBR&B tousled wayfarers kale chips ramps kitsch craft beer. Blue bottle put a bird on it deep v DIY, four loko retro distillery.
+                  </p>
+                </div>
+
+                <div id="comments" className="col s12">
+                  <h5>Comments</h5>
+                  <p>Pabst kogi cardigan echo park raw denim helvetica. Mlkshk echo park PBR&B, dreamcatcher franzen forage iPhone blog pop-up four dollar toast fixie chartreuse VHS brunch leggings. Chillwave pitchfork forage venmo, pork belly irony authentic ugh. Literally gentrify banh mi pinterest hoodie. Typewriter migas mlkshk hoodie letterpress biodiesel post-ironic. Church-key pitchfork mlkshk, cardigan everyday carry fashion axe tofu. Intelligentsia DIY shabby chic, chartreuse cardigan occupy distillery tilde artisan kogi flexitarian readymade.
+                  </p></div>
+              </div>
+            </div>
+          </div>
+       </div>
+      );
+    } else {
       return(
         <div>Nothing to display</div>
-        );
+      );
     }
   }
 
